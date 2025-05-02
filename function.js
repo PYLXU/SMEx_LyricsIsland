7084c946422fc9792184596d6edfa5a11ddf4618
PYLXU <e-pylxu@outlook.com>
2025-05-02 12:16:49 +0800

    优化代码，将歌词翻译变量声明从const改为let

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ let lastPost = null;
 const updateLyrics = () => {
   const currentLyricsElement = document.querySelector('.SimLRC>.active');
   const currentLyrics = currentLyricsElement ? currentLyricsElement.firstChild.textContent : '';
-  const currentLyricsTranslate = currentLyricsElement && currentLyrics !== currentLyricsElement.lastChild.textContent ? currentLyricsElement.lastChild.textContent : '';
+  let currentLyricsTranslate = currentLyricsElement && currentLyrics !== currentLyricsElement.lastChild.textContent ? currentLyricsElement.lastChild.textContent : '';
   let currentMusicName = document.querySelector('.musicInfo > div')?.innerHTML + " - " + document.querySelector('.musicInfo > b')?.innerHTML;
   // console.log('Lyrics:', currentLyrics);
   // console.log('Translation:', currentLyricsTranslate);
